b3ef5dfb2b258940db94cbd786ebd2316129382c
HardcodeCoder <hardcodecoder@gmail.com>
2020-08-27 17:51:13 +0530

    fixed bottom sheet not visible in landscape mode

--- app/src/main/java/com/hardcodecoder/pulsemusic/dialog/RoundedBottomSheetDialog.java
+++ app/src/main/java/com/hardcodecoder/pulsemusic/dialog/RoundedBottomSheetDialog.java
@@ -1,9 +1,12 @@
 package com.hardcodecoder.pulsemusic.dialog;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.hardcodecoder.pulsemusic.R;
 
@@ -11,5 +14,19 @@ public class RoundedBottomSheetDialog extends BottomSheetDialog {
 
     public RoundedBottomSheetDialog(@NonNull Context context) {
         super(context, R.style.BaseBottomSheetDialog);
+    }
+
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view);
+        view.getViewTreeObserver().addOnGlobalLayoutListener(() -> {
+            BottomSheetDialog dialog = RoundedBottomSheetDialog.this;
+            FrameLayout bottomSheet = dialog.findViewById(R.id.design_bottom_sheet);
+            if (null != bottomSheet) {
+                BottomSheetBehavior<FrameLayout> behavior = BottomSheetBehavior.from(bottomSheet);
+                behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                behavior.setPeekHeight(0);
+            }
+        });
     }
 }
