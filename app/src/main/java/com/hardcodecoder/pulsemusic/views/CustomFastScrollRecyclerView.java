bb92b2e3bdc49698a2d2a8843effd31d6e6da663
HardcodeCoder <hardcodecoder@gmail.com>
2020-10-07 15:30:21 +0530

    added correct color to over scroll in recycler view
    NestedScrollView does not have any public api to change over scroll edge colors, so disable over scroll mode

--- app/src/main/java/com/hardcodecoder/pulsemusic/views/CustomFastScrollRecyclerView.java
+++ app/src/main/java/com/hardcodecoder/pulsemusic/views/CustomFastScrollRecyclerView.java
@@ -2,10 +2,13 @@ package com.hardcodecoder.pulsemusic.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.EdgeEffect;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.hardcodecoder.pulsemusic.themes.ColorUtil;
 import com.hardcodecoder.pulsemusic.themes.ThemeColors;
 import com.l4digital.fastscroll.FastScrollRecyclerView;
 
@@ -30,5 +33,14 @@ public class CustomFastScrollRecyclerView extends FastScrollRecyclerView {
         setBubbleColor(accentColor);
         setHandleColor(accentColor);
         setTrackColor(ThemeColors.getCurrentColorOverlay());
+        setEdgeEffectFactory(new EdgeEffectFactory() {
+            @NonNull
+            @Override
+            protected EdgeEffect createEdgeEffect(@NonNull RecyclerView view, int direction) {
+                EdgeEffect edgeEffect = new EdgeEffect(view.getContext());
+                edgeEffect.setColor(ColorUtil.changeAlphaComponentTo(accentColor, 0.36f));
+                return edgeEffect;
+            }
+        });
     }
 }
