9314c4c67136de4a18bb364ae9442b91fc98dece
HardcodeCoder <hardcodecoder@gmail.com>
2020-10-30 16:44:11 +0530

    added bottom sheet behaviour to now playing screen
    This adds swipe/drag gesture like bottom sheet to dismiss now playing screen

--- app/src/main/java/com/hardcodecoder/pulsemusic/activities/NowPlayingScreenActivity.java
+++ app/src/main/java/com/hardcodecoder/pulsemusic/activities/NowPlayingScreenActivity.java
@@ -6,23 +6,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.hardcodecoder.pulsemusic.Preferences;
 import com.hardcodecoder.pulsemusic.R;
 import com.hardcodecoder.pulsemusic.fragments.nowplaying.screens.EdgeNowPlayingScreen;
 import com.hardcodecoder.pulsemusic.fragments.nowplaying.screens.LandscapeModeNowPlayingScreen;
 import com.hardcodecoder.pulsemusic.fragments.nowplaying.screens.ModernNowPlayingScreen;
 import com.hardcodecoder.pulsemusic.fragments.nowplaying.screens.StylishNowPlayingScreen;
+import com.hardcodecoder.pulsemusic.themes.ThemeManagerUtils;
 import com.hardcodecoder.pulsemusic.utils.AppSettings;
 
-public class NowPlayingScreenActivity extends PMBActivity {
+public class NowPlayingScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        // Apply custom theme to override transparent theme used while loading activity
+        setTheme(ThemeManagerUtils.getThemeToApply());
+        // Apply transparent theme on top of custom theme to make background transparent while dragging
+        getTheme().applyStyle(R.style.TransparentThemeOverlay, true);
+
         overridePendingTransition(R.anim.slide_in_bottom, R.anim.activity_open_exit);
+
         Fragment screenFragment;
         String tag;
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
@@ -51,8 +62,29 @@ public class NowPlayingScreenActivity extends PMBActivity {
         setContentView(R.layout.activity_now_playing_screen);
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.now_playing_screen_container, screenFragment, tag)
+                .replace(R.id.content_frame, screenFragment, tag)
                 .commit();
+        initBottomSheetBehavior();
+    }
+
+    private void initBottomSheetBehavior() {
+        FrameLayout contentFrame = findViewById(R.id.content_frame);
+        BottomSheetBehavior<FrameLayout> bottomSheetBehavior = BottomSheetBehavior.from(contentFrame);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        bottomSheetBehavior.setSkipCollapsed(true);
+        bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_HIDDEN)
+                    finish();
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                float alpha = slideOffset >= 0 ? (slideOffset + 1) / 2 : slideOffset / 2 + 0.5f;
+                bottomSheet.setAlpha(alpha);
+            }
+        });
     }
 
     private void setWindowFullScreen() {
