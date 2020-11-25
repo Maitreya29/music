96002f4e14fde39d73bee2c34cdc6185e24457a9
HardcodeCoder <hardcodecoder@gmail.com>
2020-11-25 17:07:47 +0530

    minor changes to list item view's/merged ModernImageView into MediaArtImageView

--- app/src/main/java/com/hardcodecoder/pulsemusic/views/MediaArtImageView.java
+++ app/src/main/java/com/hardcodecoder/pulsemusic/views/MediaArtImageView.java
@@ -1,6 +1,7 @@
 package com.hardcodecoder.pulsemusic.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
@@ -11,10 +12,13 @@ import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.android.material.shape.CornerFamily;
 import com.hardcodecoder.pulsemusic.GlideApp;
+import com.hardcodecoder.pulsemusic.R;
 import com.hardcodecoder.pulsemusic.helper.MediaArtHelper;
 
-public class MediaArtImageView extends ModernImageView {
+public class MediaArtImageView extends ShapeableImageView {
 
     private static final String BACKGROUND_COLOR = "#1E1E1E";
 
@@ -28,6 +32,39 @@ public class MediaArtImageView extends ModernImageView {
 
     public MediaArtImageView(Context context, @Nullable AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MediaArtImageView);
+
+        int cornerFamily = CornerFamily.ROUNDED;
+        float topLeft;
+        float topRight;
+        float bottomLeft;
+        float bottomRight;
+
+        if (typedArray.getInt(R.styleable.MediaArtImageView_cornerType, 0) == 1) {
+            cornerFamily = CornerFamily.CUT;
+        }
+
+        if (typedArray.hasValue(R.styleable.MediaArtImageView_radius)) {
+            topLeft = topRight = bottomLeft = bottomRight = typedArray.getDimension(
+                    R.styleable.MediaArtImageView_radius,
+                    context.getResources().getDimension(R.dimen.rounding_radius_4dp));
+        } else {
+            float defaultCornerSize = context.getResources().getDimension(R.dimen.rounding_radius_4dp);
+            topLeft = typedArray.getDimension(R.styleable.MediaArtImageView_radiusTopLeft, defaultCornerSize);
+            topRight = typedArray.getDimension(R.styleable.MediaArtImageView_radiusTopRight, defaultCornerSize);
+            bottomLeft = typedArray.getDimension(R.styleable.MediaArtImageView_radiusBottomLeft, defaultCornerSize);
+            bottomRight = typedArray.getDimension(R.styleable.MediaArtImageView_radiusBottomRight, defaultCornerSize);
+        }
+
+        setShapeAppearanceModel(
+                getShapeAppearanceModel()
+                        .toBuilder()
+                        .setTopLeftCorner(cornerFamily, topLeft)
+                        .setTopRightCorner(cornerFamily, topRight)
+                        .setBottomLeftCorner(cornerFamily, bottomLeft)
+                        .setBottomRightCorner(cornerFamily, bottomRight)
+                        .build());
+        typedArray.recycle();
     }
 
     public void loadAlbumArt(final String albumArtUrl, final int albumId) {
@@ -36,8 +73,10 @@ public class MediaArtImageView extends ModernImageView {
                 .listener(new RequestListener<Drawable>() {
                     @Override
                     public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                        setBackgroundColor(Color.parseColor(BACKGROUND_COLOR));
-                        setImageDrawable(MediaArtHelper.getDefaultAlbumArt(getContext(), albumId));
+                        post(() -> {
+                            setBackgroundColor(Color.parseColor(BACKGROUND_COLOR));
+                            setImageDrawable(MediaArtHelper.getDefaultAlbumArt(getContext(), albumId));
+                        });
                         return true;
                     }
 
