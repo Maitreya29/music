ffab96cc10ea18438963610d359d33a1382d0b76
HardcodeCoder <hardcodecoder@gmail.com>
2021-02-21 18:20:28 +0530

    update donation section ui/added upi option

--- app/src/main/java/com/hardcodecoder/pulsemusic/fragments/settings/SettingsDonationFragment.java
+++ app/src/main/java/com/hardcodecoder/pulsemusic/fragments/settings/SettingsDonationFragment.java
@@ -6,15 +6,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.google.android.material.button.MaterialButton;
 import com.hardcodecoder.pulsemusic.R;
 import com.hardcodecoder.pulsemusic.fragments.settings.base.SettingsBaseFragment;
-import com.hardcodecoder.pulsemusic.themes.TintHelper;
-
 
 public class SettingsDonationFragment extends SettingsBaseFragment {
 
@@ -34,14 +32,31 @@ public class SettingsDonationFragment extends SettingsBaseFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        MaterialButton coffeeBtn = view.findViewById(R.id.bmc_btn);
-        MaterialButton payPalBtn = view.findViewById(R.id.pp_btn);
 
-        TintHelper.setAccentTintToMaterialButton(coffeeBtn, false);
-        TintHelper.setAccentTintToMaterialButton(payPalBtn, false);
+        view.findViewById(R.id.bmc_frame)
+                .setOnClickListener(v -> openLink("https://www.buymeacoffee.com/HardcodeCoder"));
+        view.findViewById(R.id.pay_pal_frame)
+                .setOnClickListener(v -> openLink("https://paypal.me/HardcodeCoder"));
+        view.findViewById(R.id.upi_frame).setOnClickListener(v -> {
+            Uri uri = Uri.parse("upi://pay").buildUpon()
+                    .appendQueryParameter("pa", "ashutoshpatoa3105@oksbi")
+                    .appendQueryParameter("pn", "Pulse Music")
+                    .appendQueryParameter("cu", "INR")
+                    .build();
 
-        coffeeBtn.setOnClickListener(v -> openLink("https://www.buymeacoffee.com/HardcodeCoder"));
-        payPalBtn.setOnClickListener(v -> openLink("https://paypal.me/HardcodeCoder"));
+            Intent upiPayIntent = new Intent(Intent.ACTION_VIEW);
+            upiPayIntent.setData(uri);
+
+            // will always show a dialog to user to choose an app
+            Intent chooser = Intent.createChooser(upiPayIntent, getString(R.string.donate_options_title));
+
+            // check if intent resolves
+            if (null != chooser.resolveActivity(requireContext().getPackageManager())) {
+                startActivity(chooser);
+            } else {
+                Toast.makeText(requireContext(), getString(R.string.toast_upi_not_found), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
