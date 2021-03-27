0be018896ccd761caec94aa40b6e340f0cac6b68
HardcodeCoder <hardcodecoder@gmail.com>
2021-03-27 15:51:13 +0530

    added option to manually enter folder path in API <= 28
    Users running Android P and lower can nw enter ignore folders path manually

--- app/src/main/java/com/hardcodecoder/pulsemusic/dialog/IgnoreFolderChooser.java
+++ app/src/main/java/com/hardcodecoder/pulsemusic/dialog/IgnoreFolderChooser.java
@@ -18,10 +18,14 @@ import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.android.material.textview.MaterialTextView;
 import com.hardcodecoder.pulsemusic.R;
 import com.hardcodecoder.pulsemusic.activities.main.MediaFolderChooserActivity;
 import com.hardcodecoder.pulsemusic.adapters.bottomsheet.IgnoredFoldersAdapter;
+import com.hardcodecoder.pulsemusic.dialog.base.RoundedCustomBottomSheet;
 import com.hardcodecoder.pulsemusic.dialog.base.RoundedCustomBottomSheetFragment;
 import com.hardcodecoder.pulsemusic.providers.ProviderManager;
 import com.hardcodecoder.pulsemusic.utils.LogUtils;
@@ -59,6 +63,13 @@ public class IgnoreFolderChooser extends RoundedCustomBottomSheetFragment {
         MaterialTextView title = view.findViewById(R.id.bottom_dialog_picker_title);
         title.setText(R.string.ignored_folder_picker_title);
 
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            ImageView manualPath = view.findViewById(R.id.bottom_dialog_picker_edit);
+            manualPath.setVisibility(View.VISIBLE);
+            manualPath.setImageResource(R.drawable.ic_edit);
+            manualPath.setOnClickListener(v -> createPathInputDialog());
+        }
+
         ImageView addBtn = view.findViewById(R.id.bottom_dialog_picker_add_btn);
         addBtn.setImageResource(R.drawable.ic_folder_add);
         addBtn.setOnClickListener(v -> {
@@ -73,6 +84,29 @@ public class IgnoreFolderChooser extends RoundedCustomBottomSheetFragment {
         });
 
         ProviderManager.getIgnoredListProvider().getIgnoredList(this::setUpRecyclerView);
+    }
+
+    private void createPathInputDialog() {
+        View layout = View.inflate(requireContext(), R.layout.bottom_dialog_edit_text, null);
+        BottomSheetDialog sheetDialog = new RoundedCustomBottomSheet(layout.getContext(), RoundedCustomBottomSheet::setDefaultBehaviour);
+        sheetDialog.setContentView(layout);
+        sheetDialog.show();
+
+        ((MaterialTextView) layout.findViewById(R.id.header)).setText(getResources().getString(R.string.ignored_folder_enter_path_title));
+        ((TextInputLayout) layout.findViewById(R.id.edit_text_container)).setHint(getResources().getString(R.string.hint_ignored_folder_enter_path));
+        TextInputEditText et = layout.findViewById(R.id.text_input_field);
+
+        layout.findViewById(R.id.confirm_btn).setOnClickListener(v -> {
+            if (et.getText() == null) return;
+            String path = et.getText().toString().trim();
+            if (path.length() == 0) return;
+            handleSelectedFolderPath(path);
+            if (sheetDialog.isShowing()) sheetDialog.dismiss();
+        });
+
+        layout.findViewById(R.id.cancel_btn).setOnClickListener(v -> {
+            if (sheetDialog.isShowing()) sheetDialog.dismiss();
+        });
     }
 
     private void setUpRecyclerView(List<String> foldersList) {
@@ -103,14 +137,13 @@ public class IgnoreFolderChooser extends RoundedCustomBottomSheetFragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == REQUEST_CODE_FOLDER_SELECT) handleSelectedFolders(data);
             else if (requestCode == REQUEST_CODE_FOLDER_SELECT_Q) handleSelectedFoldersQ(data);
         }
     }
 
-    private void handleSelectedFolders(Intent data) {
+    private void handleSelectedFolders(@Nullable Intent data) {
         if (null == data || null == data.getData()) {
             Toast.makeText(requireContext(), getString(R.string.toast_failed_to_add_folder), Toast.LENGTH_SHORT).show();
             return;
@@ -120,42 +153,10 @@ public class IgnoreFolderChooser extends RoundedCustomBottomSheetFragment {
             Toast.makeText(requireContext(), getString(R.string.toast_failed_to_add_folder), Toast.LENGTH_SHORT).show();
             return;
         }
-
-        if (null == mAdapter) {
-            List<String> list = new ArrayList<>();
-            list.add(completePath);
-            setUpRecyclerView(list);
-        } else {
-            mAdapter.addItem(completePath);
-        }
-        mHasChanged = true;
-        Toast.makeText(requireContext(), getString(R.string.toast_folder_added_to_ignored_list), Toast.LENGTH_SHORT).show();
-        ProviderManager.getIgnoredListProvider().addToIgnoreList(completePath);
+        handleSelectedFolderPath(completePath);
     }
 
-    @Nullable
-    private String getPath(@NonNull Intent data) {
-        Uri folder = data.getData();
-        if (folder == null || folder.getPath() == null) return null;
-
-        String[] paths = folder.getPath().split(":");
-        String completePath = null;
-
-        if (paths.length > 0) {
-            // Length of "/tree/" = 5, we want everything from index 6
-            String volumeIdentifier = paths[0].substring(6);
-            if (volumeIdentifier.contains("primary"))
-                volumeIdentifier = "0";
-            completePath = File.separator + volumeIdentifier + File.separator
-                    // paths[1] can be null if user selected root directory
-                    + (paths.length == 1 ? "" : paths[1] + File.separator);
-        }
-        String logText = "Received path: " + folder.getPath() + "\nPaths: " + Arrays.toString(paths) + "\nCompletePath: " + completePath;
-        LogUtils.logInfo(TAG, logText);
-        return completePath;
-    }
-
-    private void handleSelectedFoldersQ(Intent data) {
+    private void handleSelectedFoldersQ(@Nullable Intent data) {
         if (null == data) {
             Toast.makeText(requireContext(), getString(R.string.toast_failed_to_add_folder), Toast.LENGTH_SHORT).show();
             return;
@@ -170,6 +171,44 @@ public class IgnoreFolderChooser extends RoundedCustomBottomSheetFragment {
         mHasChanged = true;
         Toast.makeText(requireContext(), getString(R.string.toast_folder_added_to_ignored_list), Toast.LENGTH_SHORT).show();
         ProviderManager.getIgnoredListProvider().addToIgnoreList(ignoredFoldersLList);
+    }
+
+    private void handleSelectedFolderPath(@NonNull String path) {
+        if (!path.startsWith(File.separator)) path = "" + File.separator + path;
+        if (!path.endsWith(File.separator)) path += File.separator;
+
+        LogUtils.logInfo(TAG, "Final Path: " + path);
+        if (null == mAdapter) {
+            List<String> list = new ArrayList<>();
+            list.add(path);
+            setUpRecyclerView(list);
+        } else {
+            mAdapter.addItem(path);
+        }
+        mHasChanged = true;
+        Toast.makeText(requireContext(), getString(R.string.toast_folder_added_to_ignored_list), Toast.LENGTH_SHORT).show();
+        ProviderManager.getIgnoredListProvider().addToIgnoreList(path);
+    }
+
+    @Nullable
+    private String getPath(@NonNull Intent data) {
+        Uri folder = data.getData();
+        if (folder == null || folder.getPath() == null) return null;
+
+        String[] paths = folder.getPath().split(":");
+        String completePath = null;
+
+        if (paths.length > 0) {
+            // Length of "/tree/" = 5, we want everything from index 6
+            String volumeIdentifier = paths[0].substring(6);
+            if (volumeIdentifier.contains("primary")) volumeIdentifier = "0";
+            completePath = File.separator + volumeIdentifier + File.separator
+                    // paths[1] can be null if user selected root directory
+                    + (paths.length == 1 ? "" : paths[1] + File.separator);
+        }
+        String logText = "Received path: " + folder.getPath() + "\nPaths: " + Arrays.toString(paths) + "\nCompletePath: " + completePath;
+        LogUtils.logInfo(TAG, logText);
+        return completePath;
     }
 
     @Override
